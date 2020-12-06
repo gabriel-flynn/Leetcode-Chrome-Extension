51d4bb9e75b3fb42695c4d7a5fc0fae4b1ff4a17
Gabriel Flynn <gm.flynn99@yahoo.com>
2020-12-06 07:33:08 -0600

    We now wait for jQuery to load before interacting with the DOM (#11)

--- src/popup.js
+++ src/popup.js
@@ -2,16 +2,6 @@ let addProblem = document.getElementById('addProblem');
 let removeProblem = document.getElementById('removeProblem');
 let todaysProblems = document.getElementById('todaysProblems');
 
-const pageIsLoaded = (tab) => {
-  return new Promise(resolve => {
-    chrome.tabs.onUpdated.addListener(function onUpdatedListener(tabId, info) {
-      if (tabId === tab.id && info.status === 'complete') {
-        chrome.tabs.onUpdated.removeListener(onUpdatedListener);
-        resolve();
-      }
-    });
-  });
-};
 
 addProblem.onclick = () => {
   chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
@@ -39,15 +29,14 @@ todaysProblems.onclick = async () => {
     if(!urlRegex.test(tabs[0].url)) {
       chrome.tabs.update(
           tabs[0].id,
-          {url: "https://leetcode.com/list/"}
+          {url: "https://leetcode.com/list/?"}
       );
-      await pageIsLoaded(tabs[0]);
+    } else {
+      chrome.tabs.executeScript(
+        tabs[0].id,
+        {file: 'src/todaysProblems.js'}
+      );
     }
-
-    chrome.tabs.executeScript(
-      tabs[0].id,
-      {file: 'src/todaysProblems.js'}
-    );  
   });
 };
 
--- src/todaysProblemsSamePage.js
+++ src/todaysProblemsSamePage.js
@@ -4,25 +4,12 @@
  *      todaysProblemsSamePage.js: executed when the user is already on the leetcode lists page
  */
 
-//This file gets injected onto the webpage through the inject.js file so that we are able to access the window object
 {
-    const todaysProblems = () => {
+    const todaysProblemsSamePage = async () => {
 
-        const waitForjQuery = () => {
-            return new Promise(resolve => {
-                let interval = setInterval(() => {
-                    if(window.jQuery) {
-                        clearInterval(interval);
-                        resolve();
-                    }
-                }, 2000);
-            })
-        }
-
-        await waitForjQuery();
        //Top level list div that contains all the lists
         const listDiv = document.querySelector("[class^=list-group]"); 
-
+        
        //Find the list for today
         let lists = listDiv.children;
         const todaysDate = getTodaysDateString();
@@ -45,5 +32,5 @@
         const date = new Date(Date.now());
         return `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()%100}`;
     }
-    todaysProblems();
+    todaysProblemsSamePage();
 }
